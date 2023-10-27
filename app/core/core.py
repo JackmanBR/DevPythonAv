30f2a54017ce997df2dcba5a132477a6d30a558c
paulo.guerra <paulo.guerra@alclog.com.br>
2023-10-27 18:51:44 -0300

    sistema finalizado, falta revisões finas

--- app/core/core.py
+++ app/core/core.py
@@ -1,5 +1,21 @@
-def checkProcessNumber(process):
-    processLenght = len(process)
+from app.view import home
+import time
+def registerArchivrOfData():
+    import datetime
+    import os
 
-    if process.isdigit() and processLenght == 8 and all(char.isdigit() for char in processLenght):
-        print(1)
+    currentHour = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    archivePath = os.path.join("C:\\projetos\\python\\arquivos teste", "registro.txt")
+
+    conteudoFinal = "Obrigado."
+
+    with open(archivePath, 'w') as arquivo:
+        arquivo.write(
+            "Olá usuário, \n\nSeu uso do sistema foi salvo no arquivo de registro nomeado como -registro.txt-, datado em:\n\n" + currentHour + "\n\nObrigado pelo uso deste sistema!\nPaulo Guerra.\n"+conteudoFinal)
+
+    print(f'Registro de uso salvo em: {archivePath}\n')
+
+    time.sleep(2)
+
+    home.home()
--- app/view/home.py
+++ app/view/home.py
@@ -3,15 +3,17 @@ from app.view import insertData
 from app.view import consultData
 from app.view import updateData
 from app.view import deleteData
+from app.core import core
+from app.view import home
 def home():
     while True:
         print("======================="*2, "SISTEMA DE CONTROLE PROCESSUAL","======================="*2, "\n")
-        print("1.Cadastrar processo.\n2.Consultar processo.\n3.Atualizar processo\n4.Deletar processo.\n5.Sair\n")
+        print("1.Cadastrar processo.\n2.Consultar processo.\n3.Atualizar processo\n4.Deletar processo.\n5.Salvar registro em disco.\n6.Sair\n")
         option = input("Bem vindo, por favor, digite a opcao desejada: \n\n")
 
         if option.isdigit():
             option = int(option)
-            if option >= 1 and option <= 5:
+            if option >= 1 and option <= 6:
                 if option == 1:
                     insertData.insertDataProcess()
                 if option == 2:
@@ -21,9 +23,12 @@ def home():
                 if option == 4:
                     deleteData.deleteDataProcess()
                 if option == 5:
+                    core.registerArchivrOfData()
+                    time.sleep(1)
+                if option == 6:
                     exit()
             else:
-                print("Entrada inválida. Por favor, digite um número entre 1 e 5.\n")
+                print("Entrada inválida. Por favor, digite um número entre 1 e 6.\n")
                 time.sleep(1)
         else:
             print("Entrada inválida. Por favor, digite um número entre 1 e 5.\n")
